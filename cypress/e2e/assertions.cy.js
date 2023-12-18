8938abc97451602d78b03ffd5460cc6c0ccd530a
Chinonso <chinonsochibuzor2277@gmail.comgit config --global user.email chinonsochibuzor2277@gmail.comgit config --global user.email chinonsochibuzor2277@gmail.com>
2023-12-18 18:10:18 +0100

    Explicit assertions(expect & assert)

--- cypress/e2e/assertions.cy.js
+++ cypress/e2e/assertions.cy.js
@@ -1,3 +1,6 @@
+
+
+
 describe ("Assertions", () => {
 
     it ("Implicit assertions", () => {
@@ -26,5 +29,27 @@ describe ("Assertions", () => {
 
         cy.get("input[placeholder='Username']").type("Admin")
         cy.get("input[placeholder='Username']").should('have.value', 'Admin')
+    })
+
+    it ('Explicit assertions', () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.get("input[placeholder='Username']").type("Admin")
+        cy.get("input[placeholder='Password']").type("admin123")
+        cy.get("button[type='submit']").click()
+
+        let expName = ('Test 15Jan CloudKowalski');
+
+        cy.get('.oxd-userdropdown-name').then( (x) => {
+            let actName = x.text()
+
+            //BDD Style
+            expect(actName).to.equal(expName)
+            expect(actName).to.not.equal(expName)
+
+            //TDD
+            assert.equal(actName,expName)
+            assert.notEqual(actName,expName)
+        })
+
     })
 })
